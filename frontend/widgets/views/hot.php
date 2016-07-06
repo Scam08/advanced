3c264a83f556c5e327b2aee9203a6a1c0a9174c1
Алексей <spaun08_@mail.ru>
2016-07-06 11:22:41 +0400

    Изменили цену

--- frontend/widgets/views/hot.php
+++ frontend/widgets/views/hot.php
@@ -8,7 +8,7 @@
         <div class="col-lg-4 col-sm-5"><img src="<?=\frontend\components\Common::getImageAdvert($res)[0] ?>"  class="img-responsive img-circle" alt="properties"/></div>
         <div class="col-lg-8 col-sm-7">
             <h5><a href="<?=\frontend\components\Common::getUrlAdvert($res) ?>" ><?=\frontend\components\Common::getTitleAdvert($res) ?></a></h5>
-            <p class="price">$<?=$res['price'] ?></p> </div>
+            <p class="price"><?=$res['price'] ?> руб</p> </div>
     </div>
 
     <?
